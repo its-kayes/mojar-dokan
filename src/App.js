3e70385d64efac0a9ca555bf2616a93ef75fc085
Anwar1477 <joy.cse017@gmail.com>
2022-06-19 14:04:47 +0600

    Homebar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Navbar from './components/Navbar/Navbar';
 import UserNav from './components/Navbar/UserNav';
+import Home from './components/Home/Home';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Navbar> </Navbar>
       <UserNav> </UserNav>
       <Routes>
-        <Route path='home' element={<Home> </Home>}> </Route>
+        <Route path='/' element={<Home></Home>}> </Route>
         <Route path='login' element={<Login> </Login>}>  </Route>
       </Routes>
     </div>
