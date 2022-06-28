9255dc57ad0b4c9bf6e9ca24a42ae6fb417baf40
Anwar1477 <joy.cse017@gmail.com>
2022-06-28 13:07:46 +0600

    fruits data get on mongodb server

--- src/hook/useFood.js
+++ src/hook/useFood.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../components/Shared/Loading';
 
-let loader = <p> Please Wait ... </p>
+let loader = <p> Please Wait ... </p>;
 const useFood = () => {
+  let { data: food, isLoading } = useQuery('food', () =>
+    fetch('http://localhost:8000/fruits').then((res) => res.json())
+  );
 
-    let {data: food, isLoading} = useQuery('food', ()=> fetch('food.json').then(res => res.json()));
+  if (isLoading) {
+    return <Loading> </Loading>;
+    // return loader;
+  }
 
-    if(isLoading) {
-        return <Loading> </Loading>
-        // return loader;
-    }
-
-    return food;
+  return food;
 };
 
 export default useFood;
